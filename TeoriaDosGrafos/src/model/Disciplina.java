b5731035d37557f17c8adf1cd9953336305115d8
Alexandrepsjf <alexandrepsjf@hotmail.comgit config --global user.name Alexandrepsjfgit initgit config --global user.email alexandrepsjf@hotmail.com>
2016-09-29 16:32:28 -0300

    adicionando metodos

--- TeoriaDosGrafos/src/model/Disciplina.java
+++ TeoriaDosGrafos/src/model/Disciplina.java
@@ -9,32 +9,40 @@ package model;
  *
  * @author Alexandre
  */
-class Disciplina {
-    private int ano;
-    private int periodo;
-
+public class Disciplina {
+private String nome;
+    private String ano;
+    private String periodo;
     public Disciplina() {
     }
 
-    public Disciplina(int ano, int periodo) {
+    public String getNome() {
+        return nome;
+    }
+
+    public Disciplina(String ano, String periodo) {
         this.ano = ano;
         this.periodo = periodo;
     }
 
-    public int getAno() {
+    public String getAno() {
         return ano;
     }
 
-    public void setAno(int ano) {
+    public void setAno(String ano) {
         this.ano = ano;
     }
 
-    public int getPeriodo() {
+    public String getPeriodo() {
         return periodo;
     }
 
-    public void setPeriodo(int periodo) {
+    public void setPeriodo(String periodo) {
         this.periodo = periodo;
     }
-    
+
+    public void setNome(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
--- TeoriaDosGrafos/src/view/FrmCadClienteModal.java
+++ TeoriaDosGrafos/src/view/FrmCadClienteModal.java
@@ -5,6 +5,8 @@
  */
 package view;
 
+import javax.swing.DefaultListModel;
+
 /**
  *
  * @author Sujajeb
@@ -17,6 +19,7 @@ java.awt.Frame pai;
     public FrmCadClienteModal(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
          pai= parent;
+         DefaultListModel model = new DefaultListModel();
         initComponents();
     }
 
@@ -37,9 +40,9 @@ java.awt.Frame pai;
         jLabel4 = new javax.swing.JLabel();
         disciplinaAluno = new javax.swing.JComboBox<>();
         jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        limparAlunos = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
-        jSpinner1 = new javax.swing.JSpinner();
+        anoAluno = new javax.swing.JSpinner();
 
         jTextField2.setText("jTextField2");
 
@@ -60,6 +63,11 @@ java.awt.Frame pai;
         jLabel4.setText("Disciplina:");
 
         disciplinaAluno.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        disciplinaAluno.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                disciplinaAlunoActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Cadastrar Disciplina");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -68,45 +76,46 @@ java.awt.Frame pai;
             }
         });
 
-        jButton3.setText("Limpar");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        limparAlunos.setText("Limpar");
+        limparAlunos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                limparAlunosActionPerformed(evt);
             }
         });
 
         jLabel3.setFont(new java.awt.Font("Comic Sans MS", 0, 18)); // NOI18N
         jLabel3.setText("Cadastro de Alunos");
 
-        jSpinner1.setModel(new javax.swing.SpinnerDateModel(new java.util.Date(1475167430443L), new java.util.Date(1451662980000L), null, java.util.Calendar.DAY_OF_WEEK));
+        anoAluno.setModel(new javax.swing.SpinnerDateModel(new java.util.Date(1475167430443L), new java.util.Date(1451662980000L), null, java.util.Calendar.DAY_OF_WEEK));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton3)
-                .addContainerGap())
             .addGroup(layout.createSequentialGroup()
-                .addGap(51, 51, 51)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel1))
+                        .addGap(51, 51, 51)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel1))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(nomeAluno, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(disciplinaAluno, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(anoAluno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(69, 69, 69)
+                        .addComponent(jButton1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(nomeAluno, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(disciplinaAluno, javax.swing.GroupLayout.PREFERRED_SIZE, 186, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(113, Short.MAX_VALUE))
+                        .addComponent(jButton2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(limparAlunos)))
+                .addContainerGap(102, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -120,17 +129,17 @@ java.awt.Frame pai;
                 .addGap(36, 36, 36)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(anoAluno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(38, 38, 38)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(disciplinaAluno, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel4))
-                .addGap(64, 64, 64)
+                .addGap(60, 60, 60)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(jButton2)
-                    .addComponent(jButton3))
-                .addGap(25, 25, 25))
+                    .addComponent(limparAlunos))
+                .addGap(29, 29, 29))
         );
 
         pack();
@@ -146,9 +155,18 @@ java.awt.Frame pai;
         frmCadDisciplina.setVisible(true);
     }//GEN-LAST:event_chamarTelaCadDisciplina
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void limparAlunosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_limparAlunosActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+         nomeAluno.setText("");
+        anoAluno.setValue(""); 
+        
+    }//GEN-LAST:event_limparAlunosActionPerformed
+
+    private void disciplinaAlunoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_disciplinaAlunoActionPerformed
+        // TODO add your handling code here:
+        
+        
+    }//GEN-LAST:event_disciplinaAlunoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -193,16 +211,16 @@ java.awt.Frame pai;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JSpinner anoAluno;
     private javax.swing.JComboBox<String> disciplinaAluno;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JSpinner jSpinner1;
     private javax.swing.JTextField jTextField2;
+    private javax.swing.JButton limparAlunos;
     private javax.swing.JTextField nomeAluno;
     // End of variables declaration//GEN-END:variables
 }
